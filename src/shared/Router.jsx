0e9bbde8ef16d77599332d7a91aff60446bc9cf9
Jo <johj703@nate.com>
2024-08-29 11:25:37 +0900

    add: Router에 모든 생성한 페이지 등록

---
+++ src/shared/Router.jsx
@@ -0,0 +1,34 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from '../layout/Layout';
+import Detail from '../pages/detail/Detail';
+import Main from '../pages/main/Main';
+import Login from '../pages/Login/Login';
+import Signup from '../pages/signup/Signup';
+import Write from '../pages/Write/Write';
+import Mypage from '../pages/mypage/Mypage';
+import Modify from '../pages/detail/modify/Modify';
+import Mymodify from '../pages/mypage/mymodify/Mymodify';
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Main />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/write" element={<Write />} />
+          <Route path="/mypage" element={<Mypage />}>
+            <Route path="/mymodify" element={<Mymodify />} />
+          </Route>
+
+          <Route path="/detail/:id" element={<Detail />}>
+            <Route path="/modify" element={<Modify />} />
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
