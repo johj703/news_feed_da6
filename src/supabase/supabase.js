e57f95158f6337597edafbcffc1368556edabc36
Jo <johj703@nate.com>
2024-08-29 11:56:44 +0900

    gitfix: process를 import.meta로 변경 및 export 추가

--- src/supabase/supabase.js
+++ src/supabase/supabase.js
@@ -1,4 +1,4 @@
 import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = 'https://kunfsmlsnsixsdzskbmx.supabase.co';
-const supabaseKey = process.env.SUPABASE_KEY;
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabaseKey = import.meta.env.SUPABASE_KEY;
+export const supabase = createClient(supabaseUrl, supabaseKey);
