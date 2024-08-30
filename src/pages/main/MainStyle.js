68514b0634ff0c2730dde1aba2ad56413bd9ae38
Jo <johj703@nate.com>
2024-08-30 16:26:42 +0900

    fix: jsx에서 사용하기 위해서 MainStyle.js에서 Button 컴포넌트 작성

--- src/pages/main/MainStyle.js
+++ src/pages/main/MainStyle.js
@@ -26,3 +26,18 @@ export const TableData = styled.td`
   padding: 10px;
   border-bottom: 1px solid #ccc;
 `;
+
+export const Button = styled.button`
+  padding: 10px 15px;
+  font-size: 16px;
+  color: white;
+  background-color: #007bff;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  margin-bottom: 20px; /* 버튼, 테이블 간 간격 추가 */
+  align-self: flex-start; /* 버튼 좌측 정렬 */
+  &:hover {
+    background-color: #0056b3;
+  }
+`;
