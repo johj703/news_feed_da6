f643bb30c8b4a762c98591187d77cf4be5c35d9a
GiSeong Lee <cj8928@gmail.com>
2024-08-30 14:45:06 +0900

    add: toast-ui editor 컴포넌트 생성

---
+++ src/pages/components/TuiEditor.jsx
@@ -0,0 +1,40 @@
+import { Editor } from '@toast-ui/react-editor';
+import '@toast-ui/editor/toastui-editor.css';
+import { useRef } from 'react';
+import { supabase } from '../../supabase/supabase';
+
+const toolbar = [['heading', 'bold', 'italic', 'strike'], ['hr', 'quote', 'ul', 'ol'], ['image']];
+const email = 'cj8928@gmail.com';
+const TuiEditor = ({ content, setPost, post }) => {
+  const editorRef = useRef();
+
+  const handleEditorChange = () => {
+    const editorInstance = editorRef.current.getInstance();
+    const markdownData = editorInstance.getMarkdown();
+    setPost({ ...post, contents: markdownData });
+  };
+
+  const handleImage = async (file, callback) => {
+    const { data, error } = await supabase.storage.from('post').upload(`${email}/${Date.now()}`, file);
+    callback(`https://kunfsmlsnsixsdzskbmx.supabase.co/storage/v1/object/public/${data.fullPath}`);
+    if (error) {
+      return;
+    }
+  };
+  return (
+    <Editor
+      initialValue={content ?? ' '}
+      previewStyle="vertical"
+      initialEditType="markdown"
+      autofocus={false}
+      ref={editorRef}
+      toolbarItems={toolbar}
+      hideModeSwitch
+      height="500px"
+      hooks={{ addImageBlobHook: handleImage }}
+      onChange={handleEditorChange}
+    />
+  );
+};
+
+export default TuiEditor;
