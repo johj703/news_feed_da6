d51da24498685dbe930dfe783dbb3ddedfca0074
GiSeong Lee <cj8928@gmail.com>
2024-08-30 14:44:49 +0900

    fix: outlet 제거 및 router 수정 url 수정

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -2,9 +2,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from '../layout/Layout';
 import Detail from '../pages/detail/Detail';
 import Main from '../pages/main/Main';
-import Login from '../pages/Login/Login';
+import Login from '../pages/login/Login';
 import Signup from '../pages/signup/Signup';
-import Write from '../pages/Write/Write';
+import Write from '../pages/write/Write';
 import Mypage from '../pages/mypage/Mypage';
 import Modify from '../pages/detail/modify/Modify';
 import Mymodify from '../pages/mypage/mymodify/Mymodify';
@@ -22,9 +22,8 @@ const Router = () => {
             <Route path="mymodify" element={<Mymodify />} />
           </Route>
 
-          <Route path="/detail/:id" element={<Detail />}>
-            <Route path="modify" element={<Modify />} />
-          </Route>
+          <Route path="/detail/:id" element={<Detail />} />
+          <Route path="/detail/:id/modify" element={<Modify />} />
         </Route>
       </Routes>
     </BrowserRouter>
