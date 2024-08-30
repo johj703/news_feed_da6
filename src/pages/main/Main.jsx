dedc2ba8094435c7d1e776073d1f2800d581920f
Jo <johj703@nate.com>
2024-08-30 15:13:07 +0900

    fix: MainStyle에서 작성한 컴포넌트들을 import후 임시 데이터를 작성하고 return 값에 넣어서 임시 데이터가 화면에 나타나도록 작성

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -1,5 +1,24 @@
+import { BoardContainer, Post, PostTitle, PostContent } from './MainStyle';
+
 const Main = () => {
-  return <div>홈</div>;
+  const posts = [
+    { id: 1, title: '첫 번째 게시물', content: '이건 첫 번째 게시물의 내용입니다!' },
+    { id: 2, title: '두 번째 게시물', content: '이건 두 번째 게시물의 내용입니다!' }
+  ];
+
+  console.log(posts);
+  return (
+    <BoardContainer>
+      {posts.map((post) => {
+        return (
+          <Post key={post.id}>
+            <PostTitle>{post.title}</PostTitle>
+            <PostContent>{post.content}</PostContent>
+          </Post>
+        );
+      })}
+    </BoardContainer>
+  );
 };
 
 export default Main;
