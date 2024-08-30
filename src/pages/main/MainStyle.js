59995374c876c39e7e6dfe2a582cb4b8e43971bb
Jo <johj703@nate.com>
2024-08-30 14:37:42 +0900

    add: main 페이지 style페이지 생성

---
+++ src/pages/main/MainStyle.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const BoardContainer = styled.dov`
+  padding: 20px;
+`;
+
+export const Post = styled.div`
+  border: 1px solid #ccc;
+  padding: 10px;
+  margin-bottom: 10px;
+  border-radius: 5px;
+`;
+
+export const PostTitle = styled.h3`
+  margin: 0;
+  font-size: 18px;
+`;
+
+export const PostContent = styled.p`
+  margin: 5px 0 0;
+`;
