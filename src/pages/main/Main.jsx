5344d1ee9530c2042bb596b49906a73630993c59
Jo <johj703@nate.com>
2024-08-30 15:54:34 +0900

    fix: 임시 데이터 수 늘리고, MainStyle.js에서 변경한 테이블 컴포넌트로 변경 후 임시 데이터의 속성을 나타낼 TableData 데이터 수정

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -1,9 +1,49 @@
-import { BoardContainer, Post, PostTitle, PostContent } from './MainStyle';
+import { BoardContainer, Table, TableHeader, TableRow, TableData } from './MainStyle';
 
 const Main = () => {
   const posts = [
-    { id: 1, title: '첫 번째 게시물', content: '이건 첫 번째 게시물의 내용입니다!' },
-    { id: 2, title: '두 번째 게시물', content: '이건 두 번째 게시물의 내용입니다!' }
+    {
+      id: 1,
+      title: '첫 번째 게시물',
+      content: '이건 첫 번째 게시물의 내용입니다!',
+      author: '이상해씨',
+      date: '24-08-30'
+    },
+    {
+      id: 2,
+      title: '두 번째 게시물',
+      content: '이건 두 번째 게시물의 내용입니다!',
+      author: '파이리',
+      date: '24-08-30'
+    },
+    {
+      id: 3,
+      title: '세 번째 게시물',
+      content: '이건 세 번째 게시물의 내용입니다!',
+      author: '꼬북이',
+      date: '24-08-30'
+    },
+    {
+      id: 4,
+      title: '네 번째 게시물',
+      content: '이건 네 번째 게시물의 내용입니다!',
+      author: '캐터피',
+      date: '24-08-30'
+    },
+    {
+      id: 5,
+      title: '다섯 번째 게시물',
+      content: '이건 다섯 번째 게시물의 내용입니다!',
+      author: '뿔충이',
+      date: '24-08-30'
+    },
+    {
+      id: 6,
+      title: '여섯 번째 게시물',
+      content: '이건 여섯 번째 게시물의 내용입니다!',
+      author: '구구',
+      date: '24-08-30'
+    }
   ];
 
   console.log(posts);
