aef612e97608b8b70fbba2e040d778326ef0fa1d
GiSeong Lee <cj8928@gmail.com>
2024-08-30 14:52:47 +0900

    add: 상세 페이지 테스트 데이터 생성

--- src/pages/detail/Detail.jsx
+++ src/pages/detail/Detail.jsx
@@ -1,11 +1,30 @@
-import { Outlet } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { supabase } from '../../supabase/supabase';
+import { DetailContainer, Title, TitleContainer, ViewContainer } from './DetailStyle';
+import { Viewer } from '@toast-ui/react-editor';
 
 const Detail = () => {
+  const [test, setTest] = useState('');
+
+  const post = async () => {
+    const { data } = await supabase.from('post').select('content').eq('uuid', '1d9bcb10-c706-4a42-99fb-ebfa8591b1f9');
+    setTest(data[0].content);
+  };
+
+  useEffect(() => {
+    post();
+  }, []);
+  console.log(test);
   return (
-    <div>
-      Detail
-      <Outlet />
-    </div>
+    <DetailContainer>
+      <ViewContainer className="view-container">
+        <TitleContainer>
+          <Title>title</Title>
+          <span>time</span>
+        </TitleContainer>
+        {test && <Viewer className="viewer" initialValue={test} />}
+      </ViewContainer>
+    </DetailContainer>
   );
 };
 
