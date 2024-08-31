0c5df463132b7cd6a2593d2e45994e12e0a270b2
LGS <cj8928@gmail.com>
2024-08-31 17:37:47 +0900

    delete: 데이터 가져오는 함수 제거

--- src/pages/detail/modify/Modify.jsx
+++ src/pages/detail/modify/Modify.jsx
@@ -1,19 +1,6 @@
-import { useEffect } from 'react';
-import { supabase } from '../../../supabase/supabase';
 import Form from '../../components/Form/Form';
-import { usePost } from '../../../hooks/usePost';
 
 const Modify = () => {
-  const { setPostData } = usePost();
-
-  const getPost = async () => {
-    const { data } = await supabase.from('post').select('*').eq('uuid', 'b420ecb0-9135-4761-95c0-f4e4ed57bf8c');
-    setPostData(data[0]);
-  };
-
-  useEffect(() => {
-    getPost();
-  }, []);
   return (
     <>
       <Form isModify={true} />
