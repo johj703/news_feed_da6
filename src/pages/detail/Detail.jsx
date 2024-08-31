f588d605531b1145ae0383ee09d0c449680c3c27
LGS <cj8928@gmail.com>
2024-08-31 17:35:40 +0900

    test: 로그인 수정버튼 여부 테스트 코드

--- src/pages/detail/Detail.jsx
+++ src/pages/detail/Detail.jsx
@@ -3,25 +3,47 @@ import { supabase } from '../../supabase/supabase';
 import { DetailContainer, ModifyButton, Title, TitleContainer, UserInfoContainer, ViewContainer } from './DetailStyle';
 import { Viewer } from '@toast-ui/react-editor';
 import { useLocation, useNavigate } from 'react-router-dom';
+import getPost from './hooks/getPost';
 
 const Detail = () => {
   const [test, setTest] = useState({});
   const [isAuthor, setIsAuthor] = useState(false);
   const navigate = useNavigate();
   const location = useLocation();
+
   const post = async () => {
-    const { data } = await supabase.from('post').select('*').eq('uuid', 'b420ecb0-9135-4761-95c0-f4e4ed57bf8c');
-    setTest(data[0]);
+    const response = await getPost();
+    setTest(response);
+  };
+
+  const testLoginButton = async () => {
+    let { data, error } = await supabase.auth.signInWithPassword({
+      email: 'cj8928@gmail.com',
+      password: '111111'
+    });
+    console.log(data);
+  };
+  const signUp = async () => {
+    let { data, error } = await supabase.auth.signUp({
+      email: 'cj8928@gmail.com',
+      password: '111111',
+      options: {
+        data: {
+          display_name: '이기성'
+        }
+      }
+    });
   };
 
   useEffect(() => {
     post();
+
     const testUser = async () => {
       const {
         data: { user }
       } = await supabase.auth.getUser();
 
-      if (user.user_metadata.name === test.author_name) {
+      if (user.user_metadata.display_name === test.author_name) {
         setIsAuthor(true);
       }
     };
@@ -34,6 +56,8 @@ const Detail = () => {
 
   return (
     <DetailContainer>
+      <button onClick={signUp}>회원가입</button>
+      <button onClick={testLoginButton}>로그인</button>
       <ViewContainer className="view-container">
         <TitleContainer>
           <Title>{test.title}</Title>
