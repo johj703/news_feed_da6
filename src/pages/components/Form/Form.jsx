095399f0b75995951824bdf97e349dbc6b1b5025
GiSeong Lee <cj8928@gmail.com>
2024-08-31 11:51:15 +0900

    update: form 수정 모드 코드 생성

--- src/pages/components/Form/Form.jsx
+++ src/pages/components/Form/Form.jsx
@@ -1,16 +1,39 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import TuiEditor from '../TuiEditor';
 import { useNavigate } from 'react-router-dom';
 import { ButtonContainer, FormContainer, RegisterButton, TitleInput } from './FormStyle';
 import { supabase } from '../../../supabase/supabase';
+import { usePost } from '../../../hooks/usePost';
 
 const Form = ({ isModify }) => {
   const [post, setPost] = useState({
     title: '',
     contents: ''
   });
+  const { postData } = usePost();
+  console.log(postData);
 
   const navigate = useNavigate();
+
+  const handleModifySubmit = async () => {
+    const today = new Date().toLocaleString();
+    const { error } = await supabase
+      .from('post')
+      .update([
+        {
+          author_name: '이기성',
+          author_profile_url: '',
+          date: today,
+          title: post.title,
+          content: post.contents,
+          email: 'cj8928@gmail.com'
+        }
+      ])
+      .eq('uuid', postData.uuid)
+      .select();
+    navigate('/detail/1');
+  };
+
   const handleWriteSubmit = async () => {
     const today = new Date().toLocaleString();
 
@@ -31,7 +54,7 @@ const Form = ({ isModify }) => {
     if (error) {
       console.error(error);
     }
-    isModify ? navigate('/detail/1') : navigate('/');
+    navigate('/');
   };
   const handleCancelButton = () => {
     navigate('/detail/1');
@@ -40,10 +63,16 @@ const Form = ({ isModify }) => {
     <FormContainer
       onSubmit={(e) => {
         e.preventDefault();
-        handleWriteSubmit();
+        isModify ? handleModifySubmit() : handleWriteSubmit();
       }}
     >
-      <TitleInput value={post.title} placeholder="제목" onChange={(e) => setPost({ ...post, title: e.target.value })} />
+      <TitleInput
+        value={post.title}
+        defaultValue={postData.title}
+        placeholder="제목"
+        onChange={(e) => setPost({ ...post, title: e.target.value })}
+      />
+
       <TuiEditor setPost={setPost} post={post} />
       {isModify ? (
         <ButtonContainer>
