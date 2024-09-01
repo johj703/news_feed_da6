5a5eb53bc85bbb2d2d223c3ec658790e17ff4a2a
Jo <johj703@nate.com>
2024-09-02 00:51:13 +0900

    fix: useParams로 URL에서 postId 가지고 오고, comments와 newComment State 생성

--- src/pages/comment/Comment.jsx
+++ src/pages/comment/Comment.jsx
@@ -1,4 +1,14 @@
+import { useState } from 'react';
+import { useParams } from 'react-router-dom';
+
 const Comment = () => {
+  // URL에서 postId 가지고 오기
+  const { postId } = useParams();
+
+  // State 선언: 댓글 목록과 새로운 댓글 입력값 관리
+  const [comments, setComments] = useState([]);
+  const [newComment, setNewComments] = useState('');
+
   return <div>Comment</div>;
 };
 
