3288f1a03b62c94db342bdedcabc321591cb5c3f
GiSeong Lee <cj8928@gmail.com>
2024-09-02 19:18:00 +0900

    style: 상세 페이지 버튼 스타일링 및 댓글 창 추가

--- src/pages/detail/Detail.jsx
+++ src/pages/detail/Detail.jsx
@@ -3,8 +3,9 @@ import { supabase } from '../../supabase/supabase';
 import { DetailContainer, ModifyButton, Title, TitleContainer, UserInfoContainer, ViewContainer } from './DetailStyle';
 import { Viewer } from '@toast-ui/react-editor';
 import { useLocation, useNavigate } from 'react-router-dom';
-import getPost from './hooks/getPost';
+import getPost from './components/getPost';
 import { ButtonContainer } from '../components/Form/FormStyle';
+import Comments from './components/comment/Comments';
 
 const Detail = () => {
   const [test, setTest] = useState({});
@@ -76,23 +77,31 @@ const Detail = () => {
 
   return (
     <DetailContainer>
-      <button onClick={signUp}>회원가입</button>
+      {/* <button onClick={signUp}>회원가입</button>
       <button onClick={testLoginButton}>로그인</button>
       <button onClick={logout}>로그아웃</button>
-      <button onClick={handleUserInfo}>유저정보</button>
+      <button onClick={handleUserInfo}>유저정보</button> */}
       <ViewContainer className="view-container">
         <TitleContainer>
           <Title>{test.title}</Title>
-          <span>{test.date}</span>
         </TitleContainer>
-        <UserInfoContainer>{test.author_name}</UserInfoContainer>
+        <UserInfoContainer>
+          <span>{test.author_name}</span>
+          <span>{test.date}</span>
+        </UserInfoContainer>
+
         {test.content && <Viewer className="viewer" initialValue={test.content} />}
         {isAuthor && (
           <ButtonContainer>
-            <ModifyButton onClick={() => handleModifyButton()}>수정</ModifyButton>
-            <ModifyButton onClick={() => handleDeleteButton()}>삭제</ModifyButton>
+            <ModifyButton onClick={() => handleModifyButton()} bgcolor="modify">
+              수정
+            </ModifyButton>
+            <ModifyButton onClick={() => handleDeleteButton()} bgcolor="delete">
+              삭제
+            </ModifyButton>
           </ButtonContainer>
         )}
+        <Comments />
       </ViewContainer>
     </DetailContainer>
   );
