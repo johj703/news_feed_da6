e2e1a8af705012680f99048fc88218eca3f1f55f
GiSeong Lee <cj8928@gmail.com>
2024-09-03 09:50:44 +0900

    update: 메인 게시판 구현 후 사용할 로직 주석 생성

--- src/pages/detail/components/getPost.js
+++ src/pages/detail/components/getPost.js
@@ -2,6 +2,7 @@ import { supabase } from '../../../supabase/supabase';
 
 const getPost = async (uuid) => {
   const { data } = await supabase.from('post').select('*').eq('uuid', 'f1977aec-14a4-4391-af68-f579b69d72c4');
+  // const { data } = await supabase.from('post').select('*').eq('uuid', uuid);
   return data[0];
 };
 export default getPost;
