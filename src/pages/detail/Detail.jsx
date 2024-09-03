f2d0d008e072191b6c7a499df091a17a5a4d334c
GiSeong Lee <cj8928@gmail.com>
2024-09-03 09:50:20 +0900

    update: test코드 삭제 및 user 컨텍스트 적용

--- src/pages/detail/Detail.jsx
+++ src/pages/detail/Detail.jsx
@@ -1,97 +1,56 @@
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { supabase } from '../../supabase/supabase';
 import { DetailContainer, ModifyButton, Title, TitleContainer, UserInfoContainer, ViewContainer } from './DetailStyle';
 import { Viewer } from '@toast-ui/react-editor';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
 import getPost from './components/getPost';
 import { ButtonContainer } from '../components/Form/FormStyle';
 import Comments from './components/comment/Comments';
+import { UserContext } from '../../context/UserConext';
 
 const Detail = () => {
   const [test, setTest] = useState({});
-  const [isAuthor, setIsAuthor] = useState(false);
   const navigate = useNavigate();
   const location = useLocation();
+  const params = useParams();
+  const { user } = useContext(UserContext);
 
   const getPostData = async () => {
     const response = await getPost();
     setTest(response);
   };
 
-  const testLoginButton = async () => {
-    let { data, error } = await supabase.auth.signInWithPassword({
-      email: 'cj8928@gmail.com',
-      password: '111111'
-    });
-    console.log(data);
-  };
-
-  const signUp = async () => {
-    let { data, error } = await supabase.auth.signUp({
-      email: 'cj8928@gmail.com',
-      password: '111111',
-      options: {
-        data: {
-          display_name: '이기성'
-        }
-      }
-    });
-  };
-
-  const logout = async () => {
-    let { error } = await supabase.auth.signOut();
-  };
-
-  const handleUserInfo = async () => {
-    const {
-      data: { user }
-    } = await supabase.auth.getUser();
-    console.log(user);
-  };
-
   useEffect(() => {
     getPostData();
-    const userInfo = async () => {
-      const {
-        data: { user }
-      } = await supabase.auth.getUser();
-      if (!user) {
-        return;
-      }
-      if (user.user_metadata.email === test.email) {
-        setIsAuthor(true);
-      }
-    };
-    userInfo();
-  }, [test.email]);
+  }, []);
+
   const handleModifyButton = () => {
     navigate(`${location.pathname}/modify`);
   };
 
   const handleDeleteButton = async () => {
-    console.log('삭제할거임');
-    // const { error } = await supabase.from('post').delete().eq('uuid', '');
+    const { error } = await supabase.from('post').delete().eq('uuid', params.id);
+    if (error) {
+      console.error(error);
+    }
     alert('삭제되었습니다.');
     navigate('/');
   };
 
   return (
     <DetailContainer>
-      {/* <button onClick={signUp}>회원가입</button>
-      <button onClick={testLoginButton}>로그인</button>
-      <button onClick={logout}>로그아웃</button>
-      <button onClick={handleUserInfo}>유저정보</button> */}
       <ViewContainer className="view-container">
         <TitleContainer>
           <Title>{test.title}</Title>
         </TitleContainer>
+
         <UserInfoContainer>
           <span>{test.author_name}</span>
           <span>{test.date}</span>
         </UserInfoContainer>
 
         {test.content && <Viewer className="viewer" initialValue={test.content} />}
-        {isAuthor && (
+        {user?.email === test.email && (
           <ButtonContainer>
             <ModifyButton onClick={() => handleModifyButton()} bgcolor="modify">
               수정
