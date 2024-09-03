6c4e1663e9d09027f38a8c363b61c866e241301e
GiSeong Lee <cj8928@gmail.com>
2024-09-03 09:53:15 +0900

    update: 댓글 및 유저 컨텍스트 적용 및 useCallback memo적용

--- src/pages/detail/components/comment/CommentUpdateForm.jsx
+++ src/pages/detail/components/comment/CommentUpdateForm.jsx
@@ -1,44 +1,64 @@
-import { useState } from 'react';
+import { memo, useCallback, useContext, useEffect, useState } from 'react';
 import { CommentInput, CommentInputButton, CommentInputForm } from './CommentsStyle';
+import { UserContext } from '../../../../context/UserConext';
+import { supabase } from '../../../../supabase/supabase';
+import { useParams } from 'react-router-dom';
+import { useComments } from '../../hooks/useComments';
 
-const CommentUpdateForm = ({ setIsUpdate }) => {
+const CommentUpdateForm = ({ setIsUpdate, comment, isUpdate }) => {
   const [updateComment, setUpdateComment] = useState('');
-  const handleUpdateComment = async (id) => {
-    // const { error } = await supabase
-    //   .from('comment')
-    //   .update([
-    //     {
-    //       post_id: params.id,
-    //       content: commentInput,
-    //       comment_author_name: testUser.name,
-    //       comment_author_profile_url: testUser.profile_url
-    //     }
-    //   ])
-    //   .eq('comments_id', id)
-    //   .select();
-    // if (error) {
-    //   console.error(error);
-    //   return;
-    // }
-    console.log('update');
+  const { user } = useContext(UserContext);
+  const params = useParams();
+  const { getComments } = useComments();
+
+  const handleUpdateComment = useCallback(async () => {
+    if (updateComment.length === 0) {
+      alert('댓글을 입력해주세요.');
+      return;
+    }
+    const { error } = await supabase
+      .from('comment')
+      .update([
+        {
+          content: updateComment,
+          comment_author_name: user.user_metadata.display_name,
+          comment_author_profile_url: user.user_metadata.profile_url
+        }
+      ])
+      .eq('uuid', comment.uuid)
+      .select();
+    if (error) {
+      console.error(error);
+      return;
+    }
     setIsUpdate(false);
-  };
+    getComments(params.id);
+  }, [updateComment, user, setIsUpdate, getComments, params.id, comment.uuid]);
+
+  useEffect(() => {
+    setUpdateComment(comment.content);
+  }, [comment.content]);
+
   return (
     <CommentInputForm
+      isUpdate={isUpdate}
       onSubmit={(e) => {
         e.preventDefault();
         handleUpdateComment();
       }}
     >
       <CommentInput
+        isUpdate={isUpdate}
         type="text"
         value={updateComment}
         onChange={(e) => setUpdateComment(e.target.value)}
         placeholder="댓글을 입력하세요"
       />
-      <CommentInputButton type="submit">수정</CommentInputButton>
+      <CommentInputButton isUpdate={isUpdate} type="submit">
+        수정
+      </CommentInputButton>
     </CommentInputForm>
   );
 };
 
-export default CommentUpdateForm;
+export default memo(CommentUpdateForm);
